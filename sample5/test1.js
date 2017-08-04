ea552c68bde83a10fac8ee1b73e180506cc25e28
古賀慎一 <SHIN-ICHI@kogashihinoMini.lan>
2017-08-04 16:03:35 +0900

    add qunit test

--- sample5/test1.js
+++ sample5/test1.js
@@ -1,10 +1,15 @@
 "use strict";
-QUnit.module("Test Suite 1");
-QUnit.test("Test A", function (assert) {
-    assert.ok(true, "This shouldn't fail");
-});
-QUnit.test("Test B", function (assert) {
-    assert.ok(1 === 1, "This shouldn't fail");
-    assert.ok(false, "This should fail");
+QUnit.module("App.Talk");
+QUnit.test("GetGreeting Test", function (assert) {
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 0, 0, 0, 0)), "Welcom back!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 4, 59, 59, 999)), "Welcom back!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 5, 0, 0, 0)), "Good morning!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 11, 59, 59, 999)), "Good morning!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 12, 0, 0, 0)), "Good afternoon!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 15, 59, 59, 999)), "Good afternoon!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 16, 0, 0, 0)), "Good evening!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 21, 59, 59, 999)), "Good evening!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 22, 0, 0, 0)), "Welcom back!");
+    assert.equal(App.Talk.GetGreeting(new Date(2017, 1, 1, 23, 59, 59, 999)), "Welcom back!");
 });
 //# sourceMappingURL=test1.js.map
